ffe374bfa10fa0d00f7dbd08d459d282ad89ebf1
Michael Crismali <michael.crismali@gmail.com>
2015-05-03 19:43:19 -0500

    refactored error view test

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -5,17 +5,18 @@ defmodule IvanBloggo.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(IvanBloggo.ErrorView, "404.html", []) ==
-           "Page not found"
+    assert render_template("404.html") == "Page not found"
   end
 
   test "render 500.html" do
-    assert render_to_string(IvanBloggo.ErrorView, "500.html", []) ==
-           "Internal server error"
+    assert render_template("500.html") == "Internal server error"
   end
 
   test "render any other" do
-    assert render_to_string(IvanBloggo.ErrorView, "505.html", []) ==
-           "Internal server error"
+    assert render_template("505.html") == "Internal server error"
+  end
+
+  defp render_template(name, assigns \\ []) do
+    render_to_string(IvanBloggo.ErrorView, name, assigns)
   end
 end
