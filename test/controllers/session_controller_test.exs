19c997d479286ca35387d2a4b1ea87c10f5cb4e8
Michael Crismali <michael.crismali@gmail.com>
2015-05-10 19:36:20 -0500

    converted session controller to xunit style testing syntax to see how it feels

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -15,86 +15,54 @@ defmodule IvanBloggo.SessionControllerTest do
   @user_attrs Dict.merge(@valid_params, %{"password_confirmation" => @password})
   @wrong_password_params Dict.merge(@valid_params, %{"password" => ""})
 
-  describe "#create" do
-    context "successful sign in" do
-      it "adds the user's id to the session", %{conn: conn} do
-        user = create_user(@user_attrs)
-        conn = post conn, session_path(conn, :create), @valid_params
-        assert get_session(conn, :user_id) == user.id
-      end
+  test "POST #create/2 when successful sign in", %{conn: conn} do
+    user = create_user(@user_attrs)
+    conn = post conn, session_path(conn, :create), @valid_params
 
-      it "redirects to the root path", %{conn: conn} do
-        create_user(@user_attrs)
-        conn = post conn, session_path(conn, :create), @valid_params
-        assert redirected_to(conn) == root_path(conn, :index)
-      end
-
-      it "sets the flash", %{conn: conn} do
-        create_user(@user_attrs)
-        conn = post conn, session_path(conn, :create), @valid_params
-        assert get_flash(conn, :info)
-      end
-    end
-
-    context "incorrect password" do
-      it "renders the new template with an error", %{conn: conn} do
-        create_user(@user_attrs)
-        conn = post conn, session_path(conn, :create), @wrong_password_params
-        html = html_response(conn, 200)
-        assert text_for(html, "h2") == "Sign in"
-        assert text_for(html, ".error") == "Incorrect email/password"
-      end
-
-      it "doesn't put the user id in the session", %{conn: conn} do
-        create_user(@user_attrs)
-        conn = post conn, session_path(conn, :create), @wrong_password_params
-        refute get_session(conn, :user_id)
-      end
-    end
-
-    context "incorrect email" do
-      it "renders the new template with an error", %{conn: conn} do
-        conn = post conn, session_path(conn, :create), @wrong_password_params
-        html = html_response(conn, 200)
-        assert text_for(html, "h2") == "Sign in"
-        assert text_for(html, ".error") == "Incorrect email/password"
-      end
-
-      it "doesn't put the user id in the session", %{conn: conn} do
-        conn = post conn, session_path(conn, :create), @wrong_password_params
-        refute get_session(conn, :user_id)
-      end
-    end
+    assert get_session(conn, :user_id) == user.id, "adds the user's id to the session"
+    assert redirected_to(conn) == root_path(conn, :index), "redirects to the root path"
+    assert get_flash(conn, :info), "sets the flash"
   end
 
-  describe "#new" do
-    it "renders the new template", %{conn: conn} do
-      conn = get conn, session_path(conn, :new)
-      html = html_response(conn, 200)
+  test "POST #create/2 with incorrect password", %{conn: conn} do
+    create_user(@user_attrs)
+    conn = post conn, session_path(conn, :create), @wrong_password_params
+    html = html_response(conn, 200)
 
-      assert text_for(html, "h2") == "Sign in"
-      assert attribute_for(html, "form", "action") == [session_path(conn, :create)]
-      assert text_for(html, "form label") =~ "Email"
-      assert text_for(html, "form label") =~ "Password"
-      assert attribute_for(html, "form label input", "name") == ["email", "password"]
-      assert "Sign in" in attribute_for(html, "form input", "value")
-    end
+    assert text_for(html, "h2") == "Sign in", "it renders the sign in page"
+    assert text_for(html, ".error") == "Incorrect email/password", "it renders an error"
+    refute get_session(conn, :user_id), "it does not set the flash"
   end
 
-  describe "#delete" do
-    it "clears the session", %{conn: conn} do
-      create_user(@user_attrs)
-      conn = conn
-        |> post(session_path(conn, :create), @valid_params)
-        |> delete(session_path(conn, :delete))
+  test "POST #create/2 with incorrect email", %{conn: conn} do
+    conn = post conn, session_path(conn, :create), @wrong_password_params
+    html = html_response(conn, 200)
 
-      refute get_session(conn, :user_id)
-    end
+    assert text_for(html, "h2") == "Sign in", "it renders the sign in page"
+    assert text_for(html, ".error") == "Incorrect email/password", "it renders an error"
+    refute get_session(conn, :user_id), "it does not set the flash"
+  end
 
-    it "redirects to the root", %{conn: conn} do
-      conn = delete(conn, session_path(conn, :delete))
-      assert redirected_to(conn) == root_path(conn, :index)
-    end
+  test "GET #new/2", %{conn: conn} do
+    conn = get conn, session_path(conn, :new)
+    html = html_response(conn, 200)
+
+    assert text_for(html, "h2") == "Sign in", "renders the header"
+    assert attribute_for(html, "form", "action") == [session_path(conn, :create)], "has a form"
+    assert text_for(html, "form label") =~ "Email", "it has an email field"
+    assert text_for(html, "form label") =~ "Password", "it has a password field"
+    assert attribute_for(html, "form label input", "name") == ["email", "password"], "it names the fields correctly"
+    assert "Sign in" in attribute_for(html, "form input", "value"), "it can be submitted"
+  end
+
+  test "DELETE #delete/2" do
+    create_user(@user_attrs)
+    conn = conn
+      |> post(session_path(conn, :create), @valid_params)
+      |> delete(session_path(conn, :delete))
+
+    refute get_session(conn, :user_id), "clears the session"
+    assert redirected_to(conn) == root_path(conn, :index), "redirects to root"
   end
 
   defp create_user(params) do
